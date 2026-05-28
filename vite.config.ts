492159c03ad8b5643da4ffe4255bc994db02e982
Seth <130720394+seth-c-design@users.noreply.github.com>
2026-05-28 12:06:10 -0500

    Update vite.config.ts and add GitHub Pages workflow (pages.yml)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aethera/',   // Change this to '/' if deploying to custom domain or root repo
+  base: '/aethera/',
 })
